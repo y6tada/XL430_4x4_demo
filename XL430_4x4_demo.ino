cbac9fd163dd1856b1a1ae52297ffa2a7edc19b5
y6tada <y6tada@gmail.com>
2019-06-02 20:34:21 +0900

    LED Blink during Hold-mode

--- XL430_4x4_demo.ino
+++ XL430_4x4_demo.ino
@@ -14,6 +14,10 @@ int32_t generalGoalPosition = 2048;
 
 bool isHolded = false;
 
+static int ECled_count = 0;
+static int ECled_sing  = 1;
+const  int ECled_limit = 3000;
+
 void setup() {
 	// put your setup code here, to run once:
 	dxl.begin(1000000, DXL_PACKET_VER_2_0);
@@ -34,63 +38,75 @@ void loop() {
 	// put your main code here, to run repeatedly:
 
 	updateCurrentUImode();
-	EC_setLED(curretnUImode);
 
 	if(isHolded) {
+		if()
 	    EC_update(servoSelect);
+	    ECled_count++;
+	    if(ECled_count > ECled_limit) {
+	     	ECled_count = 0;
+	     	ECled_sing *= -1;
+	    }
+	    if(ECled_sing > 0) {
+	        EC_setLED(curretnUImode);
+	    }
+	    else {
+	    	EC_setLED(turnoff);
+	    }
 	}
 	else {
+		EC_setLED(curretnUImode);
 		EC_update(servoAngle);
-	}
 
-	switch (curretnUImode) {
-		case red:
-			if(previousUImode != curretnUImode){
-				previousUImode = curretnUImode;
+		switch (curretnUImode) {
+			case red:
+				if(previousUImode != curretnUImode){
+					previousUImode = curretnUImode;
 
-				dxl.torqueOff(DXL_ALL_ID);
-				delay(10);
-				dxl.setJointExtendedMode(1);
-				delay(10);
-				dxl.torqueOn(DXL_ALL_ID);
-				delay(10);
-			}
-			dxl.setGoalPosition(1, generalGoalPosition + EC_getECcount(servoAngle) * 170);
-			break;
+					dxl.torqueOff(DXL_ALL_ID);
+					delay(10);
+					dxl.setJointExtendedMode(1);
+					delay(10);
+					dxl.torqueOn(DXL_ALL_ID);
+					delay(10);
+				}
+				dxl.setGoalPosition(1, generalGoalPosition + EC_getECcount(servoAngle) * 170);
+				break;
 
-		case green:
-			if(previousUImode != curretnUImode){
-				previousUImode = curretnUImode;
+			case green:
+				if(previousUImode != curretnUImode){
+					previousUImode = curretnUImode;
 
-				dxl.torqueOff(DXL_ALL_ID);
-				delay(10);
-				dxl.setJointExtendedMode(DXL_ALL_ID);
-				delay(10);
-				dxl.torqueOn(DXL_ALL_ID);
-				delay(10);
-			}
-			dxl.setGoalPosition(DXL_ALL_ID, generalGoalPosition + EC_getECcount(servoAngle) * 170);
-			break;
+					dxl.torqueOff(DXL_ALL_ID);
+					delay(10);
+					dxl.setJointExtendedMode(DXL_ALL_ID);
+					delay(10);
+					dxl.torqueOn(DXL_ALL_ID);
+					delay(10);
+				}
+				dxl.setGoalPosition(DXL_ALL_ID, generalGoalPosition + EC_getECcount(servoAngle) * 170);
+				break;
 
-		case blue:
-			if(previousUImode != curretnUImode){
-				previousUImode = curretnUImode;
+			case blue:
+				if(previousUImode != curretnUImode){
+					previousUImode = curretnUImode;
 
-				EC_setECspeedOffset();
+					EC_setECspeedOffset();
 
-				dxl.torqueOff(DXL_ALL_ID);
-				delay(10);
-				dxl.setWheelMode(1);
-				delay(10);
-				dxl.torqueOn(DXL_ALL_ID);
-				delay(10);
-			}
-			dxl.setGoalSpeed(1, EC_getECcountWithSpeedOffset() * 20);
-			break;
+					dxl.torqueOff(DXL_ALL_ID);
+					delay(10);
+					dxl.setWheelMode(1);
+					delay(10);
+					dxl.torqueOn(DXL_ALL_ID);
+					delay(10);
+				}
+				dxl.setGoalSpeed(1, EC_getECcountWithSpeedOffset() * 20);
+				break;
 
-		default:
-			// do something
-			break;
+			default:
+				// do something
+				break;
+		}
 	}
 }
 
@@ -110,28 +126,35 @@ void updateCurrentUImode()
 		tone(ifPin[beep], click_freq, 20);
 		// delay(20);
 
-		int encoder_sw_hold_cnt = 0;
-		while(digitalRead(ifPin[encoder_sw]) == HIGH) {
-			encoder_sw_hold_cnt++;
-			delay(10);
-			if(encoder_sw_hold_cnt > 50) {
-			    isHolded = true;
-			    tone(ifPin[beep], click_freq, 200);
-			    delay(200);
-			    while(digitalRead(ifPin[encoder_sw]) == HIGH);
-			    break;
-			}
-		}
-		delay(100);
-
 		if(isHolded) {
 		    isHolded = false;
+		    while(digitalRead(ifPin[encoder_sw]) == HIGH);
+		    delay(100);
 		}
 		else {
-			curretnUImode++;
-			if(curretnUImode > blue){
-				curretnUImode = 0;
+			int encoder_sw_hold_cnt = 0;
+			while(digitalRead(ifPin[encoder_sw]) == HIGH) {
+				encoder_sw_hold_cnt++;
+				delay(10);
+				if(encoder_sw_hold_cnt > 50) {
+				    isHolded = true;
+				    tone(ifPin[beep], click_freq, 200);
+				    delay(200);
+				    while(digitalRead(ifPin[encoder_sw]) == HIGH);
+				    delay(100);
+				}
 			}
-		}
+			if(isHolded == false) {
+			    switchUImodeToNext();
+			}
+		}		
+	}
+}
+
+void switchUImodeToNext()
+{
+	curretnUImode++;
+	if(curretnUImode > blue){
+		curretnUImode = 0;
 	}
 }
